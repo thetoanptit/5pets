9d4959be31ecb25389808ecd341aea7472aca1ac
thetoanptit <thetoan0425@gmail.com>
2017-05-15 16:10:47 +0700

    Add account model

---
+++ FindPet/Model/FPAccountModel.m
@@ -0,0 +1,13 @@
+//
+//  FPAccountModel.m
+//  FindPet
+//
+//  Created by ifis on 5/15/17.
+//  Copyright © 2017 tungnd. All rights reserved.
+//
+
+#import "FPAccountModel.h"
+
+@implementation FPAccountModel
+
+@end
