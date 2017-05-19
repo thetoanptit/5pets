143cdf3f8fc94bd08f0d30fcc6d3b47d7305ff84
thetoanptit <thetoan0425@gmail.com>
2017-05-19 22:24:10 +0700

    Change structure project and add PetModel

---
+++ FindPet/Model/Model/FPPetModel.m
@@ -0,0 +1,13 @@
+//
+//  FPPetModel.m
+//  FindPet
+//
+//  Created by ToanNT on 5/19/17.
+//  Copyright © 2017 tungnd. All rights reserved.
+//
+
+#import "FPPetModel.h"
+
+@implementation FPPetModel
+
+@end
